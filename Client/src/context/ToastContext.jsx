d8862148d6c7bfda7aa616f19d89ad2ad8178737
minhphu102003 <minhphu01200@gmail.com>
2025-02-17 22:31:52 +0700

    style(toast): customize Toast styles and animations

--- Client/src/context/ToastContext.jsx
+++ Client/src/context/ToastContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useContext } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./toast.css";
+
 import PropTypes from "prop-types";
 
 const ToastContext = createContext();
@@ -8,33 +10,17 @@ const ToastContext = createContext();
 export const useToast = () => useContext(ToastContext);
 
 export const ToastProvider = ({ children }) => {
-
-  const toastStyles = {
-    success: { backgroundColor: "#ebeced", color: "#28a745", fontWeight: "300" },
-    error: { backgroundColor: "#ebeced", color: "#dc3545", fontWeight: "300" },
-    warn: { backgroundColor: "#ebeced", color: "#ffc107", fontWeight: "300" },
-    info: { backgroundColor: "#ebeced", color: "#17a2b8", fontWeight: "300" },
-  };
-  const progressStyles = {
-    success: { backgroundColor: "#28a745", opacity: 1 },
-    error: { backgroundColor: "#dc3555", opacity: 1 },
-    warn: { backgroundColor: "#ffc107", opacity: 1 },
-    info: { backgroundColor: "#17a2b8", opacity: 1 },
-  };
-  
   const showToast = (message, type = "success") => {
     toast[type](message, {
       position: "top-right",
       autoClose: 3000,
-      hideProgressBar: false, 
+      hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
       theme: "colored",
-      style: { top: "60px", ...toastStyles[type] },
-      progressStyle: progressStyles[type], 
     });
-  };  
+  };
 
   const toastLoadingId = (message) => {
     return toast.loading(message, {
@@ -54,7 +40,7 @@ export const ToastProvider = ({ children }) => {
       closeButton: true,
       draggable: true,
       theme: "colored",
-      style: { top: "60px", ...toastStyles[type] },
+      style: { top: "60px"},
     });
   };
 
